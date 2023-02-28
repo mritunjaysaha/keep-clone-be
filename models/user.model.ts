ced026a26a2e06d3366fbea387bdb055747bf281
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-02-28 23:27:05 +0530

    feat: encryption of password

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,4 +1,6 @@
 import { model, Schema, Model, Document } from 'mongoose';
+import crypto from 'crypto';
+import { v1 as uuidV1 } from 'uuid';
 
 export interface IUser extends Document {
     firstName: string;
@@ -37,5 +39,30 @@ const UserSchema: Schema = new Schema(
     },
     { timestamps: true },
 );
+
+UserSchema.virtual('password')
+    .set(function (password) {
+        this._password = password;
+        this.salt = uuidV1();
+        this.encryptedPassword = this.securePassword(password);
+    })
+    .get(function () {
+        return this._password;
+    });
+
+UserSchema.methods = {
+    authenticate: function (plainPassword: string) {
+        return this.securePassword(plainPassword) === this.encryptedPassword;
+    },
+    securePassword: function (plainPassword: string) {
+        if (!plainPassword) return '';
+
+        try {
+            return crypto.createHmac('sha256', this.salt).update(plainPassword).digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
+};
 
 export const UserModel: Model<IUser> = model<IUser>('users', UserSchema);
