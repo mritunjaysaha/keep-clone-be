fbb66a3754404c4a0fe408b07ed522aaa8f651d4
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-05 16:52:48 +0530

    feat: controller for create todo

--- controllers/todo.controller.ts
+++ controllers/todo.controller.ts
@@ -1,3 +1,29 @@
 import { TodoModel } from '../models/todo.model';
+import { UserModel } from '../models/user.model';
 
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
+
+export const createTodo = (req: Request, res: Response) => {
+    const todo = new TodoModel(req.body);
+
+    console.log('[createTodo]', req.body);
+
+    todo.save((err, todo) => {
+        if (err) {
+            return res.status(400).json({ error: 'Failed to create todo', msg: err.message });
+        }
+
+        UserModel.findByIdAndUpdate(
+            req.profile._id,
+            { $push: { todo: todo._id } },
+            { new: true, upsert: true },
+            (err, user) => {
+                if (err || !user) {
+                    return res.status(400).json({ error: 'Failed to create todo', msg: err });
+                }
+            },
+        );
+
+        return res.json(todo);
+    });
+};
