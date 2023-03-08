cb12faf32a53ab469a31679952f68c86ab8b6c63
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-08 23:52:12 +0530

    feat: added controllers for labels

---
+++ controllers/label.controller.ts
@@ -0,0 +1,66 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { UserModel } from '../models/user.model';
+import { LabelModel } from '../models/label.model';
+
+export const getLabelById = (req: Request, res: Response, next: NextFunction, id: string) => {
+    LabelModel.findById(id).exec((err, todo) => {
+        if (err) {
+            res.status(400).json({ error: 'Label not found' });
+        }
+
+        req.label = label;
+        req.label.id = id;
+    });
+};
+
+export const getLabel = (req: Request, res: Response) => {
+    return res.json(req.label);
+};
+
+export const createLabel = (req: Request, res: Response) => {
+    const label = new LabelModel(req.body);
+
+    console.log('[createLabel]', req.body);
+
+    label.save((err, todo) => {
+        if (err) {
+            return res.status(400).json({ error: 'Failed to create label', msg: err.message });
+        }
+
+        UserModel.findByIdAndUpdate(
+            req.profile._id,
+            { $push: { label: label._id } },
+            { new: true, upsert: true },
+            (err, user) => {
+                if (err || !user) {
+                    return res.status(400).json({ error: 'Failed to create label', msg: err });
+                }
+            },
+        );
+
+        return res.json(label);
+    });
+};
+
+export const removeLabel = (req: Request, res: Response) => {
+    LabelModel.deleteOne({ _id: req.label.id }, (err, label) => {
+        if (err) {
+            res.status(400).json({ error: 'Failed to delete todo' });
+        }
+
+        return res.json({ msg: 'Label deleted successfully' });
+    });
+};
+
+export const getAllLabelByUserId = (req: Request, res: Response) => {
+    UserModel.findById(req.profile.id)
+        .populate('label')
+        .exec((err, user) => {
+            if (err) {
+                return res.status(400).json({ message: 'Failed to populate labels', error: err.message });
+            }
+
+            return res.json(user.label);
+        });
+};
