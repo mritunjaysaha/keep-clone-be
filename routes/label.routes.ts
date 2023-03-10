007a3940c25c7017876683f38196936f0102a66c
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-03-10 23:39:03 +0530

    feat: added route to remove label

--- routes/label.routes.ts
+++ routes/label.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { createLabel, getLabel, getAllLabelByUserId, getLabelById } from '../controllers/label.controller';
+import { createLabel, getLabel, getAllLabelByUserId, getLabelById, removeLabel } from '../controllers/label.controller';
 
 import { isAuthenticated, isSignedIn } from '../controllers/auth.controller';
 
@@ -10,12 +10,18 @@ import { getTodoById } from '../controllers/todo.controller';
 const router = Router();
 
 router.param('userId', getUserById);
-router.param('todoId', getTodoById);
+router.param('todoId', getLabelById);
 
 /**
  * @method POST
  * @route /api/label/:userId
  */
 router.post('/:userId', isSignedIn, isAuthenticated, createLabel);
+
+/**
+ * @method DELETE
+ * @route /api/label/:userId/:labelId
+ */
+router.delete('/:userId/:labelId', isSignedIn, isAuthenticated, removeLabel);
 
 export default router;
