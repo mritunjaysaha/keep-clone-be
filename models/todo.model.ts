1dc786cda7b054778a3b3b17657ae01cd7312cfb
mritunjaysaha <mritunjaysaha17@gmail.com>
2023-05-06 19:32:29 +0530

    feat: updated Todo interface label to labels

--- models/todo.model.ts
+++ models/todo.model.ts
@@ -1,6 +1,7 @@
 import { model, Schema, Model, Document } from 'mongoose';
 
 export interface ITodo extends Document {
+    todoId: string;
     title: string;
     body: string;
     theme: string;
@@ -8,12 +9,13 @@ export interface ITodo extends Document {
     reminder: string;
     hasReminder: boolean;
     isPinned: boolean;
-    label: string;
+    labels: string[];
     lastEdited: string;
 }
 
 const TodoSchema: Schema = new Schema(
     {
+        todoId: { type: String, required: true },
         title: { type: String, trim: true },
         body: { type: String, trim: true },
         theme: { type: String },
